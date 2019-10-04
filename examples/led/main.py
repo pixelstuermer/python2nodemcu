7ec83bb5ed851d0c58164eb09ce5eb1dea44319a
Philipp Montesano <philipp.montesano@icloud.com>
2019-10-04 15:34:41 +0200

    Harmonized code examples

--- examples/led/main.py
+++ examples/led/main.py
@@ -2,14 +2,16 @@ import time
 
 import machine
 
+SLEEP = 1.0
+
 # Pin D1 is mapped to GPIO 5
 ledPin = machine.Pin(5, machine.Pin.OUT)
 
 while True:
     ledPin.on()
     print("LED is on")
-    time.sleep(2.0)
+    time.sleep(SLEEP)
 
     ledPin.off()
     print("LED is off")
-    time.sleep(2.0)
+    time.sleep(SLEEP)
